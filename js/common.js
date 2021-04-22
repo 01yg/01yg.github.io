dfe0f0f7a930930aa80817b53119afec043f165c
jyk86 <jyk86@cyber-i.com>
2021-04-22 12:22:27 +0900

    파일명 수정

--- js/common.js
+++ js/common.js
@@ -31,30 +31,6 @@ function viewClear() {
     viewRendering('','','','');
 }
 
-/* 화면 이동 */
-function movelist() {
-    $("article").removeClass('on');
-    $("article.list.css").addClass('on');
-}
-
-/* 화면 이동 */
-function movewrite() {
-    $("article").removeClass('on');
-    $("article.write.css").addClass('on');
-}
-
-/* 화면 이동 */
-function moveview() {
-    $("article").removeClass('on');
-    $("article.view").addClass('on');
-}
-/* 화면 이동 */
-function moveup() {
-    $("article").removeClass('on');
-    $("article.up").addClass('on');
-}
-
-
 /* 현재시간 알아내기 */
 function dateToString() {
     var pDate = new Date();
---
+++ js/move.js
@@ -0,0 +1,22 @@
+/* 화면 이동 */
+function movelist() {
+    $("article").removeClass('on');
+    $("article.list.css").addClass('on');
+}
+
+/* 화면 이동 */
+function movewrite() {
+    $("article").removeClass('on');
+    $("article.write.css").addClass('on');
+}
+
+/* 화면 이동 */
+function moveview() {
+    $("article").removeClass('on');
+    $("article.view").addClass('on');
+}
+/* 화면 이동 */
+function moveup() {
+    $("article").removeClass('on');
+    $("article.up").addClass('on');
+}
