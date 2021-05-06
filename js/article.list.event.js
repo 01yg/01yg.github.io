06eddcc6a3ad9c8d063bbe6d3c091e8ca3dd5ae6
jyk86 <jyk86@cyber-i.com>
2021-05-06 09:40:02 +0900

    mobile 글쓰기

--- js/article.list.event.js
+++ js/article.list.event.js
@@ -14,3 +14,8 @@ $(document).on('keyup', 'article .list .bar input', function(e) {
         sellist($(this).val());
     }
 });
+/* 쓰기 버튼 클릭 이벤트 */
+$(document).on('click', 'article .list > button', function() {
+    writeRendering('', 'javascript', '')
+    movewrite();
+});
